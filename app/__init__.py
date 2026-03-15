37f32a1024fdd946d91fa876eecfd10a885df49c
Ken Sonza <kensonza@gmail.com>
2026-03-15 13:39:04 +0800

    update
    update

--- app/__init__.py
+++ app/__init__.py
@@ -99,4 +99,4 @@ register_blueprints_routes(app)
 
 if __name__ == '__main__':
     #socketio.run(app, debug=True, host="127.0.0.1")
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    app.run(debug=True, host="127.0.0.1")
